51bd8d8cfa53dfddf94aed6017b5bdee8ef28fe3
Mehmet Morcay <mehmetmorcay@gmail.com>
2022-02-09 21:14:53 +0100

    chore: save tmp file to same path

--- internal/persist/main.go
+++ internal/persist/main.go
@@ -3,16 +3,35 @@ package persist
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
 )
 
+var path string
+
 func Save(data interface{}) {
-	file, _ := os.Create("data.tmp")
+	file, _ := os.Create(path + "temp_bouldering.tmp")
 	defer file.Close()
 	jsonData, _ := json.MarshalIndent(data, "", " ")
 	file.Write(jsonData)
 }
 
 func Load(data interface{}) {
-	file, _ := os.ReadFile("data.tmp")
+	exec, err := os.Executable()
+	execPath := filepath.Dir(exec)
+	if err != nil {
+		panic(err)
+	}
+	file, _ := os.ReadFile(execPath + "/temp_bouldering.tmp")
+	if len(file) != 0 {
+		path = execPath + "/"
+	} else {
+		fileAtCur, _ := os.ReadFile("temp_bouldering.tmp")
+		if len(fileAtCur) != 0 {
+			file = fileAtCur
+			path = ""
+		} else {
+			path = execPath + "/"
+		}
+	}
 	json.Unmarshal(file, data)
 }
